ee33a9e305e8ce02ca2aad32b9c8d21dfd99f3f8
nanoqsh <nanoqsh@gmail.com>
2021-06-30 22:27:04 +0600

    Rename types

--- src/vec.h
+++ src/vec.h
@@ -5,11 +5,11 @@
 
 typedef struct {
     u8 *data;
-    uword cap;
-    uword len;
+    uw cap;
+    uw len;
 } vec;
 
-static vec vec_with_cap(uword cap, uword size) {
+static vec vec_with_cap(uw cap, uw size) {
     return (vec){
         .data = cap == 0 ? NULL : malloc(cap * size),
         .cap = cap,
@@ -21,7 +21,7 @@ static vec vec_new() { return vec_with_cap(0, 0); }
 
 static void vec_drop(vec *self) { free(self->data); }
 
-static void vec_drop_with(vec *self, hof *on_item, uword size) {
+static void vec_drop_with(vec *self, hof *on_item, uw size) {
     void *end = self->data + self->len * size;
     for (u8 *item = self->data; item != end; item += size) {
         hof_call(on_item, item);
@@ -30,14 +30,14 @@ static void vec_drop_with(vec *self, hof *on_item, uword size) {
     vec_drop(self);
 }
 
-static void *vec_get(vec *self, uword i, uword size) { return self->data + i * size; }
+static void *vec_get(vec *self, uw i, uw size) { return self->data + i * size; }
 
-static void vec_realloc(vec *self, uword cap, uword size) {
+static void vec_realloc(vec *self, uw cap, uw size) {
     self->data = cap == 0 ? NULL : realloc(self->data, cap * size);
     self->cap = cap;
 }
 
-static void vec_push(vec *self, void *value, uword size) {
+static void vec_push(vec *self, void *value, uw size) {
     if (self->len == self->cap) {
         vec_realloc(self, self->cap == 0 ? 4 : self->cap * 2, size);
     }
@@ -49,7 +49,7 @@ static void vec_push(vec *self, void *value, uword size) {
 
 static void vec_pop(vec *self) { self->len -= 1; }
 
-static void vec_pop_with(vec *self, hof *on_item, uword size) {
+static void vec_pop_with(vec *self, hof *on_item, uw size) {
     vec_pop(self);
     void *item = vec_get(self, self->len, size);
     hof_call(on_item, item);
